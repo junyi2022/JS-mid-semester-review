ad8e23f77d3e33356f237df3ba05d5f0f45fa3aa
junyi2022 <junyi2022@berkeley.edu>
2023-11-05 21:51:23 -0500

    week 11.1 without note

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { initializeMap } from './map.js';
+import { initializeList } from './list.js';
 import { initializeSearch } from './search.js';
 
 // get json from url
@@ -7,5 +8,23 @@ const stationInfo = await stationInfoResp.json();
 
 const events = new EventTarget(); // events object here is the event bus
 
-initializeMap(stationInfo, events); // pass event bus to each of the initialize function, so if event is fired in searchbox, map can listen to that event
+const map = initializeMap(stationInfo, events); // pass event bus to each of the initialize function, so if event is fired in searchbox, map can listen to that event
+initializeList(stationInfo, events);
 initializeSearch(stationInfo, events);
+
+function handleGeolocationSuccess(pos) {
+  console.log(pos);
+
+  const newEvent = new CustomEvent('geolocated', { detail: pos });
+  events.dispatchEvent(newEvent);
+}
+
+function handleGeolocationError(err) {
+  console.log(err);
+}
+
+navigator.geolocation.getCurrentPosition(
+  handleGeolocationSuccess,
+  handleGeolocationError);
+
+window.map = map;
--- js/map.js
+++ js/map.js
@@ -21,6 +21,62 @@ function initializeMap(stationInfo, events) {
     updateMapStations(filteredStations, stationLayer);
   });
 
+  events.addEventListener('geolocated', (evt) => {
+    // This listener will zoom to contain the three nearest stations to the
+    // user's current position.
+
+    const pos = evt.detail;
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
+
+    // Create a turf point from the user's position.
+    const userPoint = turf.point([lon, lat]);
+
+    // Create a comparison function for sorting stations by their distance from
+    // the users position.
+    function compareDists(stationA, stationB) {
+      const stationAPoint = turf.point([stationA.lon, stationA.lat]);
+      const distA = turf.distance(userPoint, stationAPoint);
+      const stationBPoint = turf.point([stationB.lon, stationB.lat]);
+      const distB = turf.distance(userPoint, stationBPoint);
+
+      return distA - distB;
+    }
+
+    // Make a copy of the stations array with the slice function, and sort it.
+    // We make a copy first here because the JS array sort function sorts the
+    // array in place, rather than returning a new array, and we want to keep
+    // the original stations array unchanged.
+    const sortedStations = stationInfo.data.stations.slice();
+    sortedStations.sort(compareDists);
+
+    // Get the three closest stations from the sorted array, and use the map
+    // function to create a multipoint containing the coordinates of those
+    // stations.
+    const closestStations = sortedStations.slice(0, 3);
+    const closestPoints = turf.multiPoint(closestStations.map((station) => [station.lon, station.lat]));
+
+    // Get the bounding box around the closest points. Turf will return this in
+    // an array of [minX, minY, maxX, maxY], so we have to convert that to a
+    // form that Leaflet is happy with (a LatLngBounds) by flipping around the X
+    // and Y (longitude and latitude) components.
+    const bbox = turf.bbox(closestPoints);
+    const leafletBbox = L.latLngBounds([bbox[1], bbox[0]], [bbox[3], bbox[2]]);
+
+    // Finally, fit the map view to the stations.
+    map.fitBounds(leafletBbox);
+  });
+
+  events.addEventListener('focus-station', (evt) => {
+    const stationId = evt.detail.stationId;
+    stationLayer.eachLayer((layer) => {
+      if (layer.stationId === stationId) {
+        layer.bindPopup('hello');
+        layer.openPopup();
+      }
+    });
+  });
+
   return map;
 }
 
@@ -41,11 +97,12 @@ function updateMapStations(stations, stationLayer) {
       alt: station.name, // for accessibility reader
       icon: stationIcon, // add customize icon
     });
-    marker.bindTooltip(station.name); // add tooltip, if data is geojson, use a function like: (m) => m.feature.properties,name)
+    marker.bindTooltip(station.name); // add tooltip, if data is geojson, use a function like: (m) => m.feature.properties.name)
     marker.bindPopup(`
       <h2 class="station-name">${station.name}</h2>
       <p class="station-address">${station.address}</p>
     `); // add popup box
+    marker.stationId = station.station_id;
     marker.addTo(stationLayer);
   };
 }
